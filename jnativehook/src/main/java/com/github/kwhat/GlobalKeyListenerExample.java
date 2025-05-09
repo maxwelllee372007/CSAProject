b72732fc0b2ecf1fe93ca9f16e0e14236bd2c92d
maxwell.lee.372007 <maxwell.lee.372007@gmail.com>
2025-05-09 07:59:45 -0400

    doesnt work

---
+++ jnativehook/src/main/java/com/github/kwhat/GlobalKeyListenerExample.java
@@ -0,0 +1,28 @@
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public class GlobalKeyListenerExample implements NativeKeyListener {
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+    }
+
+    public static void main(String[] args) {
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+            System.exit(1);
+        }
+
+        GlobalScreen.addNativeKeyListener(new GlobalKeyListenerExample());
+    }
+}
