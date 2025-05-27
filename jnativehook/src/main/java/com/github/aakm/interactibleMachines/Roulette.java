81aff4b0501accf54befa354766d13f8fa415423
Komron Beatham <Komron_Beatham@outlook.com>
2025-05-27 08:15:31 -0400

    h

--- jnativehook/src/main/java/com/github/aakm/interactibleMachines/Roulette.java
+++ jnativehook/src/main/java/com/github/aakm/interactibleMachines/Roulette.java
@@ -84,9 +84,9 @@ public class Roulette extends Machine
     private RouletteBet collectBets(Player player, KeyListener keyListener) {
         RouletteBet bet = RouletteBet.Empty();
         //Bet Evaluator Tests
-        //bet   = RouletteBet.CreateStraightUpBet(12.5, 15);
+        bet   = RouletteBet.CreateStraightUpBet(12.5, 15);
         //bet   = RouletteBet.CreateBlackOrRedBet(8.75,true);
-        bet   = RouletteBet.CreateHighOrLowBet(5.25, true);
+        //bet   = RouletteBet.CreateHighOrLowBet(5.25, true);
         //bet   = RouletteBet.CreateOddOrEvenBet(15.60, true);
         //bet   = RouletteBet.CreateDozensBet(22.75, RouletteDozens.Middle);
 
--- jnativehook/src/main/java/com/github/aakm/interactibleMachines/RouletteBet.java
+++ jnativehook/src/main/java/com/github/aakm/interactibleMachines/RouletteBet.java
@@ -23,7 +23,7 @@ public class RouletteBet
         double amount = scanner.nextDouble();
         scanner.nextLine(); // consume newline
 
-        System.out.print("Enter a number to select a bet type 1)Straight Up, 2) High Low, 3) Odd Even, 4)Color, 5) Dozens Number ");
+        System.out.print("Enter a number to select a bet type 1) Straight Up, 2) High Low, 3) Odd Even, 4)Color, 5) Dozens Number ");
         Integer betType = scanner.nextInt();
         if(betType < 1 || betType > 5)
         {
@@ -52,7 +52,7 @@ public class RouletteBet
         Integer value = scanner.nextInt();
         if(value < 1 || value > 36)
         {
-            System.out.println("Invalid bet valie! Please select a number between 1-36!");
+            System.out.println("Invalid bet! Please select a number between 1-36!");
             return new RouletteBet();
         }
         return RouletteBet.CreateStraightUpBet(amount, value);
@@ -67,12 +67,12 @@ public class RouletteBet
     }
     public static RouletteBet InputHighOrLowBet(Scanner scanner, double amount)
     {
-        System.out.println("Player has selected a Straight Up Bet!");
-        System.out.println("Please input the value to bet for your Straight Up Wager between 1-36!");
+        System.out.println("Player has selected an High or Low Bet!");
+        System.out.println("Please input the value to bet for your High or Low Wager with '1' being Low(1-18) and '2' being High(19-36)!");
         Integer value = scanner.nextInt();
-        if(value < 1 || value > 36)
+        if(value < 1 || value > 2)
         {
-            System.out.println("Invalid bet valie! Please select a number between 1-36!");
+            System.out.println("Invalid bet! Please select the value to bet for your High or Low Wager with '1' being Low(1-18) and '2' being High(19-36)!");
             return new RouletteBet();
         }
         return RouletteBet.CreateHighOrLowBet(amount, true);
@@ -88,7 +88,7 @@ public class RouletteBet
     public static RouletteBet InputOddOrEvenBet(Scanner scanner, double amount)
     {        
         System.out.println("Player has selected a Odd Or Even Bet!");
-        System.out.println("Please input an O for Odds or any other letter for Events!");
+        System.out.println("Please input an O for Odds or any other letter for Evens!");
         String value = scanner.next();
         if(value.trim().compareToIgnoreCase("o") == 0)
         {
@@ -107,12 +107,12 @@ public class RouletteBet
     public static RouletteBet InputBlackOrRedBet(Scanner scanner, double amount)
     {
         //TODO:Convert to even odd bet
-        System.out.println("Player has seltected a Straight Up Bet!");
+        System.out.println("Player has selected a colored Bet!");
         System.out.println("Please input the value to bet for your Straight Up Wager between 1-36!");
         Integer value = scanner.nextInt();
         if(value < 1 || value > 36)
         {
-            System.out.println("Invalid bet valie! Please select a number between 1-36!");
+            System.out.println("Invalid bet! Please select a number between 1-36!");
             return new RouletteBet();
         }
         return RouletteBet.CreateBlackOrRedBet(amount, true);
@@ -128,12 +128,12 @@ public class RouletteBet
     public static RouletteBet InputDozensBet(Scanner scanner, double amount)
     {
         //TODO:Convert to even odd bet
-        System.out.println("Player has seltected a Straight Up Bet!");
-        System.out.println("Please input the value to bet for your Straight Up Wager between 1-36!");
+        System.out.println("Player has seltected a dozens Bet!");
+        System.out.println("Please input the value to bet for your dozens Wager '1' being 1-12, '2' being 13-24, and '3' being 25-36!");
         Integer value = scanner.nextInt();
         if(value < 1 || value > 36)
         {
-            System.out.println("Invalid bet valie! Please select a number between 1-36!");
+            System.out.println("Invalid bet! Please select the value to bet for your dozens Wager '1' being 1-12, '2' being 13-24, and '3' being 25-36!");
             return new RouletteBet();
         }
         return RouletteBet.CreateDozensBet(amount, RouletteDozens.High);
@@ -152,7 +152,7 @@ public class RouletteBet
     {
         return this.amount;
     }
-    private RouletteBetType betType   = RouletteBetType.notSet;
+    private RouletteBetType betType = RouletteBetType.notSet;
     public RouletteBetType GetBetType()
     {
         return this.betType;
@@ -162,7 +162,7 @@ public class RouletteBet
     {
         return this.Value;
     } 
-    private RouletteDozens dozens       = RouletteDozens.notSet;
+    private RouletteDozens dozens = RouletteDozens.notSet;
     public RouletteDozens getDozens()
     {
         return this.dozens;
